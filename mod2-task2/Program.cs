b36f75ffe79c5aea55bcafbe0588030ef32cacc3
Igor Samusev <Tor1n@list.ru>
2022-01-19 21:58:51 +0200

    Add mod2task2

---
+++ mod2-task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod2_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FullName = "Николетта Фрост";
+            double PointPrograming = 56.2;
+            double PointMath = 26.8;
+            double PointPhysics = 76.3;
+            double summ = (PointPrograming + PointMath + PointPhysics)/3;
+
+            Console.WriteLine($"Испытуемый: {FullName}\nСредняя оченка эффективности: {summ}");
+            Console.WriteLine("\nДля просмотра детального отчета нажмите любую клавишу...");
+            Console.ReadKey();
+            Console.WriteLine("\nПрограммирование: {0}\nФизика: {1}\nМатематика: {2}", 
+                PointPrograming, 
+                PointPhysics, 
+                PointMath);
+            Console.ReadKey();
+        }
+    }
+}
