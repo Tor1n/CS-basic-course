23aa75242dfe99c6c4f75c7b3743e122400edcae
Igor Samusev <Tor1n@list.ru>
2022-01-26 01:25:21 +0200

    mod3-task3 add to correct folder

---
+++ Module3/mod3-task1_/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите цисло, чтобы определить чётность: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine("0 не может быть преобразован");
+            }
+            else if (number % 2 == 0)
+            {
+                Console.WriteLine("Вы ввели чётное число");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели нечётное число");
+            }
+            Console.ReadKey();
+        }
+    }
+}
