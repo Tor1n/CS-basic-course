e96789843b77e2400becfd22002e8f8ae1d06178
Igor Samusev <Tor1n@list.ru>
2022-01-26 01:05:54 +0200

    mod3-task3 added

---
+++ Module3/mod3-task3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod3_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чтобы проверить является ли число простым, введите целое число:");
+            int number = int.Parse(Console.ReadLine());
+
+            bool condition = false;
+            while (number > 0)
+            {                
+                    for (int k = 2; k < number -1; k++)
+                    {
+                        if (number % k == 0)
+                        {
+                           condition = true;
+                           break;
+                         }
+                    Console.WriteLine($"k >>> {k}");
+                    }
+                
+            }
+
+            if (condition = true)
+            {
+                Console.WriteLine($"К сожалению число {number} не является простым");
+            }
+            else
+            {
+                Console.WriteLine("Ура! число простое");
+            }
+            Console.ReadKey();
+        }
+    }
+}
