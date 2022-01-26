8995c1c7829c7037d589919d6bf40f32ff89b2b5
Igor Samusev <Tor1n@list.ru>
2022-01-26 12:05:27 +0200

    +task1 fixed
    q

--- Module3/mod3-task1_/Program.cs
+++ Module3/mod3-task1_/Program.cs
@@ -4,41 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task3
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Укажите цисло, чтобы определить простое ли оно: ");
+            Console.WriteLine("Укажите цисло, чтобы определить чётность: ");
             int number = int.Parse(Console.ReadLine());
-            int k = 2;
-            bool condition = false;
-            while (k < number)
-            {
-                
-                
-                    
-                    if (number % k == 0)
-                    {
-                        condition = true;
-                        break;
 
-                }
-                else
-                {
-                    k++;
-                    Console.WriteLine($"k сейчас:{k}");
-                }
-                
-            }
-            if (condition == false)
+            if (number == 0)
             {
-                Console.WriteLine("Число не является простым");
+                Console.WriteLine("0 не может быть преобразован");
+            }
+            else if (number % 2 == 0)
+            {
+                Console.WriteLine("Вы ввели чётное число");
             }
             else
             {
-                Console.WriteLine("Число простое");
+                Console.WriteLine("Вы ввели нечётное число");
             }
             Console.ReadKey();
         }
