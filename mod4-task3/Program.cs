1fe4b56335868371546647b2acddad1409d6307c
Samusiev <i.samusiev@pampamstudio.com>
2022-02-09 17:50:54 +0200

    task2-3

--- mod4-task3/Program.cs
+++ mod4-task3/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace mod4_task1
+namespace mod4_task3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
         }
     }
 }
