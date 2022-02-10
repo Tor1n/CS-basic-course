78fa6150ff8769d1b42d2120df1c7637219a553b
Samusiev <i.samusiev@pampamstudio.com>
2022-02-10 18:09:58 +0200

    4-1 done, 4-2 almost done

--- Module4/mod4-task1/Program.cs
+++ Module4/mod4-task1/Program.cs
@@ -10,7 +10,28 @@ namespace mod4_task1
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Укажите цисло строк в матрице: ");
+            int row = int.Parse(Console.ReadLine());
+
+            Console.Write("Укажите цисло столбцов в матрице: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[row, col];
+            Random r = new Random();
+            int summ = 0;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = r.Next(10);
+                    Console.Write($"{matrix[i, j]} ");
+                    summ = summ + matrix[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.Write($"Сумма всех элементов матрицы: {summ}");
+            Console.ReadKey(); 
         }
     }
 }
--- mod4-task2/Program.cs
+++ mod4-task2/Program.cs
@@ -10,6 +10,22 @@ namespace mod4_task2
     {
         static void Main(string[] args)
         {
+            Console.Write("Укажите длинну последовательности: ");
+            int sequence = int.Parse(Console.ReadLine());
+            int[] row = new int[sequence];
+
+            Console.WriteLine($"Введите {sequence} чисел");
+            for (int i = 0; i < sequence; i++)
+            {
+                row[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (var item in row)
+            {
+                Console.Write($"{item} ");
+            }
+            int max = row.Max();
+            Console.Write($"Максимальное значение последовательности: {max}");
+            Console.ReadKey();
         }
     }
 }
