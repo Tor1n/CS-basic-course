dd7676962851f95c20ac53062f21c973b36e8afb
Samusiev <i.samusiev@pampamstudio.com>
2022-02-11 17:52:58 +0200

    all done

--- mod4-task3/Program.cs
+++ mod4-task3/Program.cs
@@ -10,6 +10,42 @@ namespace mod4_task3
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите максимальное число диапазона: ");
+            int maxvalue = int.Parse(Console.ReadLine());
+            Random r = new Random();
+            int secretnum = r.Next(maxvalue + 1);
+            
+            Console.WriteLine($"Теперь отгадайте случайное число от 0 до {maxvalue}");
+            string variant = (Console.ReadLine());
+            int userinput = int.Parse($"{variant}");
+            while (true)
+            {
+
+                if (variant == " ")
+                {
+                    break;
+                }
+                else if (userinput > secretnum)
+
+                {
+                    Console.WriteLine("Нет, загаданное число меньше");
+                    userinput = int.Parse(Console.ReadLine());
+                }
+
+                else if (userinput < secretnum)
+                {
+                    Console.WriteLine("Нет, загаданное число больше");
+                    userinput = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Ура!");
+                    break;
+                }
+
+            }
+            Console.Write($"Загаданное число: {secretnum}");
+            Console.ReadKey();
         }
     }
 }
