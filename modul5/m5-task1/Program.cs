a98cc45bebb16a24f10c8bc77598a92dec1b4d30
Samusiev <i.samusiev@pampamstudio.com>
2022-02-17 16:06:05 +0200

    m5-task1 added

---
+++ modul5/m5-task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m5_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите предложение: ");
+            string userInput = Console.ReadLine();
+
+            string[] words = Split(userInput);
+
+            Console.WriteLine("\nПеречень слов из предложения: ");
+            Print(words);
+                        
+            Console.ReadKey();
+        }
+        static string[] Split(string text)
+        {
+            string[] words = text.Split(' ');
+            return words;
+
+        }
+        static void Print(string[] outputWords)
+        {
+            foreach (var item in outputWords)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
