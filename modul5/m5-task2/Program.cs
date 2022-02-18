d57211de80e5a8f152c8c2f31793f073ba3668d4
Samusiev <i.samusiev@pampamstudio.com>
2022-02-18 16:14:58 +0200

    m5-task2 done

---
+++ modul5/m5-task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m5_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите предложение: ");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine(ReversWords(userInput));
+
+
+
+            Console.ReadKey();
+        }
+        static string[] Split(string text)
+        {
+            string[] words = text.Split(' ');
+            return words;
+
+        }
+        
+        static string ReversWords(string input)
+        {
+            string[] words;
+            
+            words = Split(input);
+
+            Array.Reverse(words);
+            string finalPhrase = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+               finalPhrase += words[i] + " ";
+            }
+            return finalPhrase;
+        }
+    }
+}
