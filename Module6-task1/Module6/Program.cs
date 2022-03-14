12e57aef2dcd34d81a188dcff76c16af1f394de8
Igor Samusev <i.samusev.sp@gmail.com>
2022-03-14 21:28:28 +0200

    init m6

---
+++ Module6-task1/Module6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace module6
+{
+    internal class program
+    {
+        static void main (string[] args)
+        {
+            Console.WriteLine("hello world!");
+        }
+    }
+}
