6097d661cbe9518d8b4cebc2fb4fb2d43de7bae0
Igor Samusev <i.samusev.sp@gmail.com>
2022-03-24 19:58:55 +0300

    Complete Mody7

--- Module6-task1/Mod7Template/Program.cs
+++ Module6-task1/Mod7Template/Program.cs
@@ -13,118 +13,8 @@ namespace Mod7Template
         {
             string path = @"Сотрудники.txt";
             Check(path);
-            //if (!File.Exists(path)) //Создаем дефолтный файл в случае отсутсвия
-            //{
-            //    using (File.Create(path));
-            //    File.AppendAllText(path, "0#23.03.2022 18:41:08#Илья Щербин#41#185#18.11.1981#Севастополь\n" +
-            //        "1#20.12.2021 00:12#Иванов Иван Иванович#30#176#05.05.1992#Москва\n" +
-            //        "2#15.12.2021 03:12#Алексеев Алексей Иванович#44#176#05.11.1980#Томск\n");
-            //}
-            
             Repository rep = new Repository(path); //Заносим данные из файла в буфер
             Action(rep);
-            //Console.WriteLine("Выбирите действие:\n1.Просмотр данных\n2.Добавление новой записи\n" +
-            //    "3.Редактирование записи по ID\n4.Cортировать по дате рождения\n5.Cортировать по дате добавления\n" +
-            //    "6.Записи добавленные за период\n7.Удаление записи по ID");
-
-            //int key = int.Parse(Console.ReadLine());
-            //switch (key)
-            //{
-            //    case 1: //Выводим все данные из буфера на экран
-            //        rep.PrintDbToConsole(); 
-            //        Console.WriteLine($"Записей в файле: {rep.Count}");
-            //        break;
-            //    case 2: //Добавляем новую запись
-            //        Console.Write("Имя: ");
-            //        string newName = Console.ReadLine();
-            //        Console.Write("Возраст: ");
-            //        uint newAge = uint.Parse(Console.ReadLine());
-            //        Console.Write("Рост: ");
-            //        uint newHeight = uint.Parse(Console.ReadLine());
-            //        Console.Write("Дата Рождения: ");
-            //        DateTime newBirthDate = DateTime.Parse(Console.ReadLine());
-            //        Console.Write("Место Рождения: ");
-            //        string newPlace = Console.ReadLine();
-
-            //        rep.Add(new Worker((uint)rep.Count, DateTime.Now, newName, newAge, newHeight, newBirthDate, newPlace));
-            //        Console.Write("Запись добавлена");
-            //        break;
-            //    case 3: //Редактируем запись по ID
-            //        Console.WriteLine("Укажите ID сотрудника, данные которого необходимо отредактировать: ");
-            //        int editID = int.Parse(Console.ReadLine());
-            //        rep.PrintDataByID(editID);
-            //        Console.WriteLine("Какие данные необходимо отредактировать?" +
-            //            "\n1.Имя\n2.Возраст\n3.Рост\n4.Дату рождения\n5.Место рождения");
-            //        int editKey = int.Parse(Console.ReadLine());
-            //        switch (editKey)
-            //        {
-            //            case 1:
-            //                Console.Write("Введите новые данные имени: ");
-            //                string nName = Console.ReadLine();
-            //                rep.EditName(editID, nName);
-            //                Console.WriteLine("Данные успешно перезаписаны.");
-            //                break;
-            //            case 2:
-            //                Console.Write("Введите новые данные возраста: ");
-            //                uint nAge = uint.Parse(Console.ReadLine());
-            //                rep.EditAge(editID, nAge);
-            //                Console.WriteLine("Данные успешно перезаписаны.");
-            //                break;
-            //            case 3:
-            //                Console.WriteLine("Введите новые данные роста: ");
-            //                uint nHeight = uint.Parse(Console.ReadLine());
-            //                rep.EditHeight(editID, nHeight);
-            //                Console.WriteLine("Данные успешно перезаписаны.");
-            //                break;
-            //            case 4:
-            //                Console.WriteLine("Введите новую дату рождения: ");
-            //                DateTime nBDate = DateTime.Parse(Console.ReadLine());
-            //                rep.EditBDate(editID, nBDate);
-            //                Console.WriteLine("Данные успешно перезаписаны.");
-            //                break;
-            //            case 5:
-            //                Console.WriteLine("Введите новое место рождения: ");
-            //                string nBPlace = Console.ReadLine();
-            //                rep.EditBPlace(editID, nBPlace);
-            //                Console.WriteLine("Данные успешно перезаписаны.");
-            //                break;
-            //            default:
-            //                break;
-            //        }
-
-            //        break;
-            //    case 4:
-            //        rep.SortByBDate();
-            //        break;
-            //    case 5:
-            //        rep.SortByDateAdded();
-            //        break;
-            //    case 6:
-            //        Console.WriteLine("Дата от которой вывести записи: ");
-            //        DateTime from = DateTime.Parse(Console.ReadLine());
-            //        Console.WriteLine("Дата до которой вывести записи: ");
-            //        DateTime till = DateTime.Parse(Console.ReadLine());
-            //        rep.PrintDbByPeriod(from, till);
-            //        break;
-            //    case 7:
-            //        rep.PrintDbToConsole();
-            //        Console.WriteLine("введите ID записи которую необходимо удалить:");
-                                        
-            //        break;
-            //    default:
-            //        break;
-            //}
-            //if (key == 7)
-            //{
-            //    uint delindex = uint.Parse(Console.ReadLine());
-            //    rep.DeletionSave("Сотрудники.txt", delindex);
-            //    Console.WriteLine("Запить успешно удалена.");
-            //}
-            //else
-            //{
-            //    rep.Save("Сотрудники.txt");
-            //}
-
             Console.ReadKey();
         }
         static void Action(Repository rep) //Метод вырора действия пользователем
@@ -220,18 +110,8 @@ namespace Mod7Template
                     break;
             }
             Finish(key, rep);
-            //if (key == 7)
-            //{
-            //    uint delindex = uint.Parse(Console.ReadLine());
-            //    rep.DeletionSave("Сотрудники.txt", delindex);
-            //    Console.WriteLine("Запить успешно удалена.");
-            //}
-            //else
-            //{
-            //    rep.Save("Сотрудники.txt");
-            //}
         }
-        static void Finish(int key, Repository rep)
+        static void Finish(int key, Repository rep) //Метод сохранения изменений из буфера в файл
         {
             if (key == 7)
             {
@@ -244,9 +124,9 @@ namespace Mod7Template
                 rep.Save("Сотрудники.txt");
             }
         }
-        static void Check(string path)
+        static void Check(string path) //Создаем дефолтный файл в случае отсутсвия
         {
-            if (!File.Exists(path)) //Создаем дефолтный файл в случае отсутсвия
+            if (!File.Exists(path)) 
             {
                 using (File.Create(path)) ;
                 File.AppendAllText(path, "0#23.03.2022 18:41:08#Илья Щербин#41#185#18.11.1981#Севастополь\n" +
